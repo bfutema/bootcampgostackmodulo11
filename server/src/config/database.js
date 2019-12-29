df35553259fb87eb6106054a820ad7ae108555a0
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-12-29 20:18:00 -0300

    Teste de criação de usuário

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,12 +1,28 @@
-require('../bootstrap');
+// require('../bootstrap');
+
+// module.exports = {
+//   dialect: process.env.DB_DIALECT || 'postgres',
+//   host: process.env.DB_HOST,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   storage: './__tests__/database.sqlite',
+//   logging: false,
+//   define: {
+//     timestamps: true,
+//     underscored: true,
+//     underscoredAll: true,
+//   },
+// };
 
 module.exports = {
-  dialect: process.env.DB_DIALECT || 'postgres',
+  dialect: 'sqlite',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   storage: './__tests__/database.sqlite',
+  logging: false,
   define: {
     timestamps: true,
     underscored: true,
